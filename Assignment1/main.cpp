77181e4ce86094c7a7d5a8f34abf9c8a5144a478
TheEnabled <sol_tabboga@dlsu.edu.ph>
2025-01-21 20:56:37 +0800

    done

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,7 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
 
 
 int main(void) {
@@ -22,23 +23,53 @@ int main(void) {
     glfwMakeContextCurrent(window);
     gladLoadGL();
 
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shapes,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    std::vector<GLuint> mesh_indices;
+    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(shapes[0].mesh.indices[i].vertex_index);
+    }
+
+
     GLfloat vertices[]{
         0.f,    0.5f,   0.f
         -0.5f,  0.f,    0.f,
         0.5f,   0.f,    0.f
     };
 
-    GLuint VAO, VBO;
+    GLuint indices[]{
+        0, 1, 2
+    };
+
+
+
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
+
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBufferData(
         GL_ARRAY_BUFFER,
-        sizeof(vertices),
-        vertices,
+        sizeof(GLfloat) * attributes.vertices.size(),
+        &attributes.vertices[0],
         GL_STATIC_DRAW
         //GL_DYNAMIC_DRAW if needs to move.
         );
@@ -52,9 +83,19 @@ int main(void) {
         (void*)0
     );
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                sizeof(GLuint) * mesh_indices.size(),
+                mesh_indices.data(),
+                GL_STATIC_DRAW
+        );
+
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     glBindVertexArray(0);
 
 
@@ -65,7 +106,7 @@ int main(void) {
 
         glBindVertexArray(VAO);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -76,6 +117,7 @@ int main(void) {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
